c8cb4e2c13c5254771bf26cfe271de79b79c7a79
oodzfa <72552431+oodzfa@users.noreply.github.com>
2024-12-07 18:47:17 +0800

    添加项目文件。

---
+++ MinecraftServerConsole/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Windows;
+
+namespace MinecraftServerConsole
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        public static string java_path { get; set; }
+        public static string server_file { get; set; }
+        public static string mem { get; set; }
+        public static bool settings_window_opened { get; set; }
+
+        public App()
+        {
+            try
+            {
+                if (!Directory.Exists("server"))
+                {
+                    Directory.CreateDirectory("server");
+                }
+                JObject config = JObject.Parse(File.ReadAllText("config.json"));
+                java_path = config["java_path"].ToString();
+                server_file = config["server_file"].ToString();
+                mem = config["mem"].ToString();
+            }
+            catch { }
+        }
+    }
+}
---
+++ MinecraftServerConsole/SettingsWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using MahApps.Metro.Controls;
+using Microsoft.Win32;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Windows;
+
+namespace MinecraftServerConsole
+{
+    /// <summary>
+    /// SettingsWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class SettingsWindow : MetroWindow
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+            javaBox.Text = App.java_path;
+            fileBox.Text = App.server_file;
+            memBox.Text = App.mem;
+        }
+
+        private void javaButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "java可执行文件|java.exe";
+            if (ofd.ShowDialog() == true)
+            {
+                javaBox.Text = ofd.FileName;
+            }
+        }
+
+        private void MetroWindow_Closed(object sender, EventArgs e)
+        {
+            try
+            {
+                App.java_path = javaBox.Text;
+                App.server_file = fileBox.Text;
+                App.mem = memBox.Text;
+                JObject obj = new JObject();
+                obj["java_path"] = App.java_path;
+                obj["server_file"] = App.server_file;
+                obj["mem"] = App.mem;
+                File.WriteAllText("config.json", obj.ToString());
+            } catch {}
+            App.settings_window_opened = false;
+        }
+    }
+}
